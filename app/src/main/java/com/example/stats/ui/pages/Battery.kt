56dc111cccc7affe100ddfcdadbc4861c7b3f18d
ByteManager <110998030+kel2v@users.noreply.github.com>
2026-01-31 03:19:14 +0530

    different code for currentNow polling and intent subscription, but same problem

--- app/src/main/java/com/example/stats/ui/pages/Battery.kt
+++ app/src/main/java/com/example/stats/ui/pages/Battery.kt
@@ -4,68 +4,23 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.stats.ui.listitem.ListItem
 
 @Composable
 fun Battery(batteryViewModel: BatteryViewModel = viewModel()) {
-    val currentNow by batteryViewModel.currentNowStateFlow.collectAsState()
     val batteryState by batteryViewModel.batteryStateStateFlow.collectAsState()
+    val currentNow by batteryViewModel.currentNowStateFlow.collectAsState()
+
     Column {
 
-        val list = listOf(
-            listOf(
-                "Battery type",
-                batteryState.technology?:"null"
-            ),
-
-            listOf(
-                "Battery level",
-                batteryState.level?.toString() + "%"
-            ),
-
-            listOf(
-                "Battery voltage",
-                batteryState.voltage?.toString() + " V"
-            ),
-
-            listOf(
-                "Battery current (now)",
-                currentNow.toString() + "mA"
-            ),
-
-            listOf(
-                "Battery temperature",
-                batteryState.temperature?.toString() + " Celsius"
-            ),
-
-            listOf(
-                "Battery health",
-                batteryState.health?:"null"
-            ),
-
-            listOf(
-                "Charging status",
-                batteryState.chargingStatus?:"null"
-            )
-        )
-
-        list.forEach {
-            ListItem(
-                modifier = Modifier,
-                parameterName = it[0],
-                parameterValue = it[1]
-            )
-        }
-
-//        Text("Battery type = ${batteryState.technology}")
-//        Text("Battery level = ${batteryState.level}%")
-//        Text("Battery voltage = ${batteryState.voltage} V")
-//        Text("Battery current (now) = $currentNow mA")
-//        Text("Battery temperature = ${batteryState.temperature} Celsius")
-//        Text("Battery health = ${batteryState.health}")
-//        Text("Charging status = ${batteryState.chargingStatus}")
+        ListItem(parameterName = "battery type", parameterValue = batteryState.technology?:"null")
+        ListItem(parameterName = "Battery level", parameterValue = "${batteryState.level}")
+        ListItem(parameterName = "Battery voltage", parameterValue = "${batteryState.voltage/1000f} V")
+        ListItem(parameterName = "Battery current (now)", parameterValue = "$currentNow mA")
+        ListItem(parameterName = "Battery temperature", parameterValue = "${batteryState.temperature/10f} Celsius")
+        ListItem(parameterName = "Battery health", parameterValue = "${batteryState.health}")
+        ListItem(parameterName = "Charging status", parameterValue = "${batteryState.chargingStatus}")
     }
 
 }
--- app/src/main/java/com/example/stats/ui/pages/BatteryViewModel.kt
+++ app/src/main/java/com/example/stats/ui/pages/BatteryViewModel.kt
@@ -9,13 +9,17 @@ import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.stateIn
-import java.util.Timer
-import java.util.TimerTask
+import kotlinx.coroutines.launch
 
 object CurrentNowRepository {
     private lateinit var appContext: Context
@@ -24,27 +28,27 @@ object CurrentNowRepository {
         appContext = context.applicationContext
     }
 
+    val appScope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
+
     val currentNowStateFlow by lazy {
-        callbackFlow {
-            val timer = Timer()
-            val task = object : TimerTask() {
-                override fun run() {
-                    val batteryManager =
-                        appContext.getSystemService(Context.BATTERY_SERVICE) as BatteryManager
-                    val currentNow: Int =
-                        batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_NOW) / 1000
-                    trySend(currentNow)
-                }
-            }
 
-            timer.schedule(task, 0, 1000)
+        flow {
+            val batteryManager =
+                appContext.getSystemService(Context.BATTERY_SERVICE) as BatteryManager
 
-            awaitClose {
-                timer.cancel()
+            while (true) {
+                emit(
+                    batteryManager
+                        .getIntProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_NOW) / 1000
+                )
+                delay(1_000)
             }
-        }.stateIn(
-            scope = CoroutineScope(SupervisorJob() + Dispatchers.Default),
-            started = SharingStarted.WhileSubscribed(1000),
+        }
+        .flowOn(Dispatchers.IO)
+        .distinctUntilChanged()
+        .stateIn(
+            scope = appScope,
+            started = SharingStarted.WhileSubscribed(1_000),
             initialValue = 0
         )
     }
@@ -57,70 +61,48 @@ object BatteryStateRepository {
         appContext = context.applicationContext
     }
 
+    val appScope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
+
     val batteryStateStateFlow by lazy {
         callbackFlow {
             val receiver = object : BroadcastReceiver() {
                 override fun onReceive(context: Context?, intent: Intent?) {
                     if(intent == null) return
 
-                    val level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
-                    val scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
-                    val percent = (level * 100) / scale
-
-                    val status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1)
-                    val charging = when(status) {
-                        BatteryManager.BATTERY_STATUS_CHARGING -> "Charging"
-                        BatteryManager.BATTERY_STATUS_NOT_CHARGING -> "Not charging"
-                        BatteryManager.BATTERY_STATUS_FULL -> "Fully charged"
-                        BatteryManager.BATTERY_STATUS_DISCHARGING -> "Discharging"
-                        else -> "Unknown"
-                    }
-
-                    val temp = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, -1) / 10f
-
-                    val voltage = intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, -1) / 1000f
-
-                    val technology = intent.getStringExtra(BatteryManager.EXTRA_TECHNOLOGY)
-
-                    val health = when(intent.getIntExtra(BatteryManager.EXTRA_HEALTH, BatteryManager.BATTERY_HEALTH_UNKNOWN)) {
-                        BatteryManager.BATTERY_HEALTH_GOOD -> "Good"
-                        BatteryManager.BATTERY_HEALTH_OVERHEAT -> "Overheating"
-                        BatteryManager.BATTERY_HEALTH_DEAD -> "Dead"
-                        BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE -> "Over voltage"
-                        BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE -> "Unspecified failure"
-                        BatteryManager.BATTERY_HEALTH_COLD -> "Cold"
-                        else -> "Unknown"
-                    }
-
-                    trySend(
-                        BatteryState(
-                            level = percent,
-                            voltage = voltage,
-                            chargingStatus = charging,
-                            temperature = temp,
-                            technology = technology,
-                            health = health
+                    appScope.launch {
+                        val state = BatteryState(
+                            level = (intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100) / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1),
+                            chargingStatus = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1),
+                            temperature = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, -1),
+                            voltage = intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, -1),
+                            technology = intent.getStringExtra(BatteryManager.EXTRA_TECHNOLOGY),
+                            health = intent.getIntExtra(BatteryManager.EXTRA_HEALTH, BatteryManager.BATTERY_HEALTH_UNKNOWN)
                         )
-                    )
+
+                        trySend(state)
+                    }
+
+
                 }
             }
 
             appContext.registerReceiver(receiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
 
             awaitClose {
+                appScope.cancel()
                 appContext.unregisterReceiver(receiver)
             }
 
         }.stateIn(
-            scope = CoroutineScope(SupervisorJob() + Dispatchers.Default),
+            scope = appScope,
             started = SharingStarted.WhileSubscribed(1000),
             initialValue = BatteryState(
-                level = null,
-                voltage = null,
-                chargingStatus = null,
-                temperature = null,
+                level = Int.MIN_VALUE,
+                voltage = Int.MIN_VALUE,
+                chargingStatus = Int.MIN_VALUE,
+                temperature = Int.MIN_VALUE,
                 technology = null,
-                health = null
+                health = Int.MIN_VALUE
             )
         )
     }
@@ -133,10 +115,10 @@ class BatteryViewModel() : ViewModel() {
 }
 
 data class BatteryState(
-    val level: Int? = null,
-    val voltage: Float? = null,
-    val chargingStatus: String? = null,
-    val temperature: Float? = null,
-    val technology: String? = null,
-    val health: String? = null
+    val level: Int,
+    val voltage: Int,
+    val chargingStatus: Int,
+    val temperature: Int,
+    val technology: String?,
+    val health: Int
 )
