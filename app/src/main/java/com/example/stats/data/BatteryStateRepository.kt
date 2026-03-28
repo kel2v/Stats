41e7e4781fadf12ff64708b9ef1d589d0fdbf1ed
ByteManager <110998030+kel2v@users.noreply.github.com>
2026-03-28 20:20:07 +0530

    batteryStateFlow will be initialized normally (no lazy{}).

--- app/src/main/java/com/example/stats/data/BatteryStateRepository.kt
+++ app/src/main/java/com/example/stats/data/BatteryStateRepository.kt
@@ -22,56 +22,50 @@ class BatteryStateRepository @Inject constructor(@ApplicationContext private val
 
     // This ia a Flow object that emits BatteryState whenever new Intent indicating Intent.ACTION_BATTERY_CHANGED is broadcasted.
     // It is defined lazy, means it is initialized on its first access.
-    val batteryStateFlow by lazy {
+    val batteryStateFlow = callbackFlow {
+        Log.d("PERMISSION DIALOG", "callbackFlow is started executing")
 
-        // callbackFlow creates a Flow object that reacts to new events it is concerned with by emitting a value.
-        // Here, on receiving a broadcast for Intent.ACTION_BATTERY_CHANGED, it emits a BatteryState object with new values initialized as provided by the broadcast.
-        callbackFlow {
-            Log.d("PERMISSION DIALOG", "callbackFlow is started executing")
+        // creates an object of type BroadcastReceiver that also overrides `onReceive` method, which is called by Android when any Intent of Intent.ACTION_BATTERY_CHANGED type occurs
+        val receiver = object : BroadcastReceiver() {
 
-            // creates an object of type BroadcastReceiver that also overrides `onReceive` method, which is called by Android when any Intent of Intent.ACTION_BATTERY_CHANGED type occurs
-            val receiver = object : BroadcastReceiver() {
+            // It is called by the Android whenever an Intent of Intent.ACTION_BATTERY_CHANGED type occurs
+            override fun onReceive(context: Context?, intent: Intent?) {
+                if(intent == null) return
 
-                // It is called by the Android whenever an Intent of Intent.ACTION_BATTERY_CHANGED type occurs
-                override fun onReceive(context: Context?, intent: Intent?) {
-                    if(intent == null) return
+                val state = BatteryState(
+                    level = (intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100) / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1),
+                    temperature = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, -1) / 10f,
+                    voltage = intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, -1)/1000f,
+                    technology = intent.getStringExtra(BatteryManager.EXTRA_TECHNOLOGY),
 
-                    val state = BatteryState(
-                        level = (intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100) / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1),
-                        temperature = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, -1) / 10f,
-                        voltage = intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, -1)/1000f,
-                        technology = intent.getStringExtra(BatteryManager.EXTRA_TECHNOLOGY),
+                    health = when (intent.getIntExtra(BatteryManager.EXTRA_HEALTH, BatteryManager.BATTERY_HEALTH_UNKNOWN)) {
+                        BatteryManager.BATTERY_HEALTH_COLD -> "Cold"
+                        BatteryManager.BATTERY_HEALTH_DEAD -> "Dead"
+                        BatteryManager.BATTERY_HEALTH_GOOD -> "Good"
+                        BatteryManager.BATTERY_HEALTH_OVERHEAT -> "Overheat"
+                        BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE -> "Overvoltage"
+                        BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE -> "Unspecified failure"
+                        BatteryManager.BATTERY_HEALTH_UNKNOWN -> "Unknown"
+                        else -> "Unknown"
+                    },
+                    chargingStatus = when(intent.getIntExtra(BatteryManager.EXTRA_STATUS, BatteryManager.BATTERY_STATUS_UNKNOWN)) {
+                        BatteryManager.BATTERY_STATUS_FULL -> "Full"
+                        BatteryManager.BATTERY_STATUS_CHARGING -> "Charging"
+                        BatteryManager.BATTERY_STATUS_DISCHARGING -> "Discharging"
+                        BatteryManager.BATTERY_STATUS_NOT_CHARGING -> "Not charging"
+                        BatteryManager.BATTERY_STATUS_UNKNOWN -> "Unknown"
+                        else -> "Unknown"
+                    }
+                )
 
-                        health = when (intent.getIntExtra(BatteryManager.EXTRA_HEALTH, BatteryManager.BATTERY_HEALTH_UNKNOWN)) {
-                            BatteryManager.BATTERY_HEALTH_COLD -> "Cold"
-                            BatteryManager.BATTERY_HEALTH_DEAD -> "Dead"
-                            BatteryManager.BATTERY_HEALTH_GOOD -> "Good"
-                            BatteryManager.BATTERY_HEALTH_OVERHEAT -> "Overheat"
-                            BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE -> "Overvoltage"
-                            BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE -> "Unspecified failure"
-                            BatteryManager.BATTERY_HEALTH_UNKNOWN -> "Unknown"
-                            else -> "Unknown"
-                        },
-                        chargingStatus = when(intent.getIntExtra(BatteryManager.EXTRA_STATUS, BatteryManager.BATTERY_STATUS_UNKNOWN)) {
-                            BatteryManager.BATTERY_STATUS_FULL -> "Full"
-                            BatteryManager.BATTERY_STATUS_CHARGING -> "Charging"
-                            BatteryManager.BATTERY_STATUS_DISCHARGING -> "Discharging"
-                            BatteryManager.BATTERY_STATUS_NOT_CHARGING -> "Not charging"
-                            BatteryManager.BATTERY_STATUS_UNKNOWN -> "Unknown"
-                            else -> "Unknown"
-                        }
-                    )
-
-                    trySend(state)
-                }
+                trySend(state)
             }
+        }
 
-            appContext.registerReceiver(receiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+        appContext.registerReceiver(receiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
 
-            awaitClose {
-                appContext.unregisterReceiver(receiver)
-            }
-
+        awaitClose {
+            appContext.unregisterReceiver(receiver)
         }
     }
 
